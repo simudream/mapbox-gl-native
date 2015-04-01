640a7d19bb53e96f342db48f69fceddaf8af8268
Justin R. Miller <incanus@codesorcery.net>
2015-04-01 11:36:18 -0700

    flesh out Travis matrix

--- scripts/test_ios.sh
+++ scripts/test_ios.sh
@@ -8,5 +8,4 @@ xctool test \
     -project ./test/ios/ios-tests.xcodeproj \
     -scheme 'Mapbox GL Tests' \
     -sdk iphonesimulator \
-    -destination 'platform=iOS Simulator,name=iPhone 5s,OS=latest' \
-    -destination-timeout 1
+    -destination "platform=iOS Simulator,name=${1} ${2},OS=${3}"
